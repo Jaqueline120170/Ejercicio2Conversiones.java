c646cff00cc1a0558e252b771c277023713605b4
Jaqueline Romero <csi23-jromres@colegioaltair.net>
2023-11-01 18:44:55 +0100

    cambios

---
+++ src/ejercicio2Conversiones/Conversiones.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package Ejercicio2Conversiones;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class Ejercicio2Conversiones {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Decimal --> Double
+		
+		double=numeroDouble ();
+		 numeroDouble= deDecimalADouble ();
+		 
+		// Double--> Int
+		 
+		 double numero;
+			System.out.println("Introduce un número");
+			Scanner numeroIntroducido = new Scanner(System.in);
+			numero = numeroIntroducido.nextDouble();
+			int numeroEntero = (int) numero;
+		 
+		 
+		
+
+	}
+	
+	
+
+}
